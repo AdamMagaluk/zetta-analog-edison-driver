20042112e7dd26d422122ca5cdb4bddbf04332ac
Adam Magaluk <AdamMagaluK@gmail.com>
2014-10-29 15:10:18 -0400

    Updated example server

--- examples/server.js
+++ examples/server.js
@@ -3,6 +3,4 @@ var Analog = require('../index');
 
 zetta()
   .use(Analog, 0)
-  .listen(1337, function(){
-    console.log('Zetta is running at http://beaglebone.local:1337');
-  });
+  .listen(1337);
